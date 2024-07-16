4c0dfddda3bab44db618d49a146d47e3eb2b15a0
marquinf1 <marcos.ferraz@ufvjm.edu.br>
2024-07-16 00:02:31 -0300

    import { SpeedInsights } from "@vercel/speed-insights/next"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function Home() {
   return (
     <div>
       <h1>Home e Gestão</h1>
+      <SpeedInsights />
     </div>
   );
 }
